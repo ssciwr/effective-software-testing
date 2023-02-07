6422b9d048c512fa23bca7bfbca9545384d2ffc9
Liam Keegan <liam@keegan.ch>
2023-02-07 16:45:22 +0100

    add test for attempted move on already taken square

--- tests/test_board.py
+++ tests/test_board.py
@@ -4,10 +4,11 @@ import pytest
 
 board_sizes = [(1, 1), (2, 2), (1, 2), (2, 1), (3, 3)]
 valid_players = [Player.CROSS, Player.CIRCLE]
+invalid_players = [Player.EMPTY]
 
 
 @pytest.mark.parametrize("rows,cols", board_sizes)
-def test_empty_rows_x_cols_board(rows: int, cols: int) -> None:
+def test_empty_board(rows: int, cols: int) -> None:
     board = Board(rows=rows, cols=cols)
     assert board.n_cols == cols
     assert board.n_rows == rows
@@ -18,7 +19,7 @@ def test_empty_rows_x_cols_board(rows: int, cols: int) -> None:
 
 @pytest.mark.parametrize("player", valid_players)
 @pytest.mark.parametrize("rows,cols", board_sizes)
-def test_empty_1x1_board_make_move_valid(rows: int, cols: int, player: Player) -> None:
+def test_empty_board_make_move_valid(rows: int, cols: int, player: Player) -> None:
     for row in range(rows):
         for col in range(cols):
             board = Board(rows, cols)
@@ -27,21 +28,37 @@ def test_empty_1x1_board_make_move_valid(rows: int, cols: int, player: Player) -
             assert board.square(row, col) == player
 
 
+@pytest.mark.parametrize("invalid_player", invalid_players)
 @pytest.mark.parametrize("rows,cols", board_sizes)
-def test_empty_1x1_board_make_move_invalid_player(rows: int, cols: int) -> None:
+def test_empty_board_make_move_invalid_player(
+    rows: int, cols: int, invalid_player: Player
+) -> None:
     for row in range(rows):
         for col in range(cols):
             board = Board(rows, cols)
             assert board.square(row, col) == Player.EMPTY
-            assert board.make_move(row, col, Player.CROSS) is False
+            assert board.make_move(row, col, invalid_player) is False
             assert board.square(row, col) == Player.EMPTY
 
 
 @pytest.mark.parametrize("player", valid_players)
 @pytest.mark.parametrize("row,col", [(-1, -2), (6, 7), (5, 4), (4, 4)])
 @pytest.mark.parametrize("rows,cols", board_sizes)
-def test_empty_1x1_board_make_move_invalid_row_col(
+def test_empty_board_make_move_invalid_square(
     player: Player, rows: int, cols: int, row: int, col: int
 ) -> None:
     board = Board(rows, cols)
     assert board.make_move(row, col, player) is False
+
+
+@pytest.mark.parametrize("player1", valid_players)
+@pytest.mark.parametrize("player2", valid_players)
+@pytest.mark.parametrize("rows,cols", board_sizes)
+def test_make_move_square_already_taken(
+    player1: Player, player2: Player, rows: int, cols: int
+) -> None:
+    for row in range(rows):
+        for col in range(cols):
+            board = Board(rows, cols)
+            assert board.make_move(row, col, player1) is True
+            assert board.make_move(row, col, player2) is False
