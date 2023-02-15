08825dfe3295c4f16c61bf9afe22f4c59d965ea7
Liam Keegan <liam@keegan.ch>
2023-02-15 18:44:56 +0100

    add pytest-qt for qt tests

--- tests/test_board_widget.py
+++ tests/test_board_widget.py
@@ -2,14 +2,14 @@ from effective_software_testing.board import Board
 from effective_software_testing.board_widget import BoardWidget
 from PyQt6.QtCore import QSize
 from PyQt6.QtGui import qRgb
-from PyQt6.QtWidgets import QApplication
+from typing import Any
 
 
-def test_empty_board_widget() -> None:
-    app = QApplication([])
+def test_empty_board_widget(qtbot: Any) -> None:
     board = Board(3)
     board_widget = BoardWidget(board)
     board_widget.show()
+    qtbot.addWidget(board_widget)
     widget_size = QSize(103, 87)
     board_widget.resize(widget_size)
     image = board_widget.pixmap().toImage()
