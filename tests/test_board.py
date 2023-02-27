c7d904d2f854548377e136bc8d256c7fa69497e5
Liam Keegan <liam@keegan.ch>
2023-02-27 11:31:27 +0100

    parametrize players for valid move test

--- tests/test_board.py
+++ tests/test_board.py
@@ -20,17 +20,21 @@ def test_empty_board_invalid_squares() -> None:
     assert "col" in str(e.value)
 
 
-def test_empty_board_make_valid_move() -> None:
+@pytest.mark.parametrize("row", [0, 1, 2])
+@pytest.mark.parametrize("col", [0, 1, 2])
+@pytest.mark.parametrize("player", [p for p in Player])
+def test_empty_board_make_valid_move(row: int, col: int, player: Player) -> None:
     board = Board()
-    assert board.square(0, 0) is None
-    assert board.make_move(0, 0, Player.CROSS) is True
-    assert board.square(0, 0) == Player.CROSS
+    assert board.square(row, col) is None
+    assert board.make_move(row, col, player) is True
+    assert board.square(row, col) == player
 
 
-def test_empty_board_make_move_invalid_square() -> None:
+@pytest.mark.parametrize("player", [p for p in Player])
+def test_empty_board_make_move_invalid_square(player: Player) -> None:
     board = Board()
     with pytest.raises(BoardException) as e:
-        board.make_move(-1, 0, Player.CROSS)
+        board.make_move(-1, 0, player)
     assert "row" in str(e.value)
 
 
@@ -38,4 +42,4 @@ def test_empty_board_make_move_square_taken() -> None:
     board = Board()
     assert board.make_move(0, 0, Player.CROSS) is True
     assert board.make_move(0, 0, Player.CROSS) is False
-    assert board.make_move(0, 0, Player.CROSS) is False
+    assert board.make_move(0, 0, Player.CIRCLE) is False
