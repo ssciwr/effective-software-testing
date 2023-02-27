08439c9b1d23f2a3859597a0467cc95eb15006a7
Liam Keegan <liam@keegan.ch>
2023-02-27 11:23:31 +0100

    add make_move() to Board

--- effective_software_testing/board.py
+++ effective_software_testing/board.py
@@ -1,5 +1,5 @@
 from effective_software_testing.player import Player
-from typing import Optional
+from typing import Optional, List
 
 
 class BoardException(ValueError):
@@ -9,7 +9,9 @@ class BoardException(ValueError):
 class Board:
     def __init__(self) -> None:
         self.n = 3
-        self.squares = [[None for _ in range(self.n)] for _ in range(self.n)]
+        self.squares: List[List[Optional[Player]]] = [
+            [None for _ in range(self.n)] for _ in range(self.n)
+        ]
 
     def square(self, row: int, col: int) -> Optional[Player]:
         """If a `Player` has made a move at (`row`,`col`) returns the `Player`, otherwise returns `None`"""
@@ -18,3 +20,10 @@ class Board:
         if not 0 <= col < self.n:
             raise BoardException(f"Invalid col {col}")
         return self.squares[col][row]
+
+    def make_move(self, row: int, col: int, player: Player) -> bool:
+        """If possible, make move for `Player` at (`row`,`col`) and return `True`, otherwise return `False`."""
+        if self.square(row, col) is None:
+            self.squares[col][row] = player
+            return True
+        return False
--- tests/test_board.py
+++ tests/test_board.py
@@ -1,4 +1,5 @@
 from effective_software_testing.board import Board, BoardException
+from effective_software_testing.player import Player
 import pytest
 
 
@@ -17,3 +18,15 @@ def test_empty_board_invalid_squares() -> None:
     with pytest.raises(BoardException) as e:
         board.square(0, -2)
     assert "col" in str(e.value)
+
+
+def test_empty_board_make_valid_move() -> None:
+    board = Board()
+    assert board.make_move(0, 0, Player.CROSS) is True
+
+
+def test_empty_board_make_move_invalid_square() -> None:
+    board = Board()
+    with pytest.raises(BoardException) as e:
+        board.make_move(-1, 0, Player.CROSS)
+    assert "row" in str(e.value)
