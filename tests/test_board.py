6fb2004d7c7ecd9f6adca9219759b0738767e1a2
Liam Keegan <liam@keegan.ch>
2023-02-27 10:46:38 +0100

    add test for invalid square

--- tests/test_board.py
+++ tests/test_board.py
@@ -1,6 +1,14 @@
 from effective_software_testing.board import Board
+import pytest
 
 
-def test_empty_board() -> None:
+def test_empty_board_valid_squares() -> None:
     board = Board()
     assert board.get_square(0, 0) is None
+    assert board.get_square(2, 2) is None
+
+
+def test_empty_board_invalid_squares() -> None:
+    board = Board()
+    with pytest.raises(Exception):
+        board.get_square(3, 3)
